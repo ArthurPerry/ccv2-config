61872c877aa88b6be08bee09ec7f29a3060de73f
Dobbs, Brendan <brendan.dobbs@sap.com>
2020-02-05 15:44:20 +0000

    OData fix for supertypes

---
+++ kymafixes/src/de/hybris/platform/odata2services/odata/persistence/lookup/CustomItemLookupStrategy.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package de.hybris.platform.odata2services.odata.persistence.lookup;
+
+import de.hybris.platform.integrationservices.search.FlexibleSearchQueryBuilder;
+import de.hybris.platform.integrationservices.search.ItemTypeMatch;
+import de.hybris.platform.odata2services.odata.persistence.ItemLookupRequest;
+
+import org.apache.olingo.odata2.api.edm.EdmException;
+
+
+/**
+ *
+ */
+public class CustomItemLookupStrategy extends DefaultItemLookupStrategy
+{
+	@Override
+	protected FlexibleSearchQueryBuilder queryBuilder(final ItemLookupRequest lookupRequest) throws EdmException
+	{
+		final FlexibleSearchQueryBuilder builder = super.queryBuilder(lookupRequest);
+		builder.withTypeHierarchyRestriction(ItemTypeMatch.ALL_SUBTYPES);
+		return builder;
+	}
+}
